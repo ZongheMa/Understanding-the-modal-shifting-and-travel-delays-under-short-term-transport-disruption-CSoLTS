fd1b45ab0dab6c1bb064942afa2445ba0d7134b0
Elio-Zonghe <elioma@163.com>
2024-01-08 21:29:50 +0000

    Section updates
    Data preparation completed

--- defined_methods.py
+++ defined_methods.py
@@ -38,4 +38,28 @@ def merge_csv(directory):
     # Return the merged dataframe
     return merged_df
 
-# decompression('/Users/zonghe/Library/CloudStorage/OneDrive-Personal/Paper/Tube Strike Behaviour/travel_info_oa')
+def merge_csv_flow(directory):
+    # Get a list of all the csv files
+    csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
+
+    # Initialize an empty list to hold dataframes
+    dfs = []
+
+    # Loop through csv files, read each into a dataframe, and append to the list
+    for file in csv_files:
+        # Extract date from filename, assuming the date is in format 'traffic_flow_YYYY_MM_DD'
+        date_str = file.split('.')[0].split('_')[-3:]  # This gives ['YYYY', 'MM', 'DD']
+        date = datetime.strptime('_'.join(date_str), '%Y_%m_%d').date()
+
+        df = pd.read_csv(os.path.join(directory, file))
+
+        # Add date as a new column
+        df['date'] = date.strftime('%m/%d/%y')
+
+        dfs.append(df)
+
+    # Concatenate all dataframes in the list into one dataframe
+    merged_df = pd.concat(dfs, ignore_index=True).drop_duplicates()
+
+    # Return the merged dataframe
+    return merged_df
